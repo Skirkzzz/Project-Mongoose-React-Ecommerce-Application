1a50e19df0552d3203f1b8eb53e3687c7c765fdf
Your Namels <selina.kirk82@gmail.com>
2022-08-20 14:19:12 +0100

    coding descriptions

--- Client/src/data/products.js
+++ Client/src/data/products.js
@@ -1,23 +1,66 @@
 const products = [
   {
-    title: "Art 1",
-    description: "Spectacular piece by British designer The Don",
-    image: "https://picsum.photos/200",
+    title: "AlienLanguage",
+    description:
+      "Denotes am impressionist view of the language of the ancient human race written in React using mongoose",
+    image: "XXXXXX",
     price: "$50,000",
   },
   {
-    title: "Art 2",
+    title: "FromAncientTimes",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
-    image: "https://picsum.photos/200",
-    price: "$100",
+      "Rudimentary HTML and javascript code lovingly brushed with hues of green and blue, signalling the birth of code from the first coder Ada Lovelace",
+    image: "XXXXXX",
+    price: "$100,000",
+  },
+
+  {
+    title: "InTheBeginning",
+    description: "Impressionist view of cave art, written in typescript",
+    image: "XXXXXX",
+    price: "$70,000",
   },
   {
-    title: "Product 3",
+    title: "PatchworkCode",
+    description:
+      "Impressionist art depicting the emotional journey in code, when applications are updated and parts of the code die and require a rebirth",
+    image: "XXXXXX",
+    price: "$120,000",
+  },
+  {
+    title: "TheAztecs",
+    description:
+      "Ancient Aztec scribes sensitively depicted in typescript. Hues of green are explored to explore the art of the ancient natural world",
+    image: "https://picsum.photos/200",
+    price: "$85,000",
+  },
+  {
+    title: "TheGenius",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
     image: "https://picsum.photos/200",
-    price: "$100",
+    price: "$65,000",
+  },
+  {
+    title: "TheHole",
+    description:
+      "Lovingly painted in Python. 'The Genius' shows a natural coder able to stray beyond the standard lines of reality and able to create something beautiful in code",
+    image: "XXXXXX",
+    price: "$70,000",
+  },
+  {
+    title: "TheMatrix",
+    description:
+      "'The Matrix' displays the monotonous and standardised reasoning of code. Whilst the colours show the love of code being brought to life'",
+    image: "XXXXXX",
+    price: "$150,000",
+  },
+  {
+    title: "TheRegenerator",
+    description:
+      "The regenerator shows one colourful and brilliant coding language dying to make way for the birth of another.",
+    image: "XXXXXX",
+    price: "$100,000",
   },
 ];
 

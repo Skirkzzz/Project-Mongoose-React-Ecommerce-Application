ca478018004b601a7b91aa94c402328a3417cb6b
Your Namels <selina.kirk82@gmail.com>
2022-08-23 19:36:40 +0100

    Update images

--- Client/src/data/products.js
+++ Client/src/data/products.js
@@ -10,56 +10,56 @@ const products = [
     title: "FromAncientTimes",
     description:
       "Rudimentary HTML and javascript code lovingly brushed with hues of green and blue, signalling the birth of code from the first coder Ada Lovelace",
-    image: "/assets/FromAncientTimes.png",
+    image: "/assets/images/FromAncientTimes.png",
     price: "$100,000",
   },
 
   {
     title: "InTheBeginning",
     description: "Impressionist view of cave art, written in typescript",
-    image: "/assets/InTheBeginning.png",
+    image: "/assets/images/InTheBeginning.png",
     price: "$70,000",
   },
   {
     title: "PatchworkCode",
     description:
       "Impressionist art depicting the emotional journey in code, when applications are updated and parts of the code die and require a rebirth",
-    image: "/assets/PatchworkCod.png",
+    image: "/assets/images/PatchworkCod.png",
     price: "$120,000",
   },
   {
     title: "TheAztecs",
     description:
       "Ancient Aztec scribes sensitively depicted in typescript. Hues of green are explored to explore the art of the ancient natural world",
-    image: "/assets/TheAztecs.png",
+    image: "/assets/images/TheAztecs.png",
     price: "$85,000",
   },
   {
     title: "TheGenius",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
-    image: "/assets/TheGenius.png",
+    image: "/assets/images/TheGenius.png",
     price: "$65,000",
   },
   {
     title: "TheHole",
     description:
       "Lovingly painted in Python. 'The Genius' shows a natural coder able to stray beyond the standard lines of reality and able to create something beautiful in code",
-    image: "/assets/TheHole.png",
+    image: "/assets/images/TheHole.png",
     price: "$70,000",
   },
   {
     title: "TheMatrix",
     description:
       "'The Matrix' displays the monotonous and standardised reasoning of code. Whilst the colours show the love of code being brought to life'",
-    image: "/assets/TheMatrix.png",
+    image: "/assets/images/TheMatrix.png",
     price: "$150,000",
   },
   {
     title: "TheRegenerator",
     description:
       "The regenerator shows one colourful and brilliant coding language dying to make way for the birth of another.",
-    image: "/assets/TheRegenerator",
+    image: "/assets/images/TheRegenerator",
     price: "$100,000",
   },
 ];

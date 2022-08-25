7d8fedcc6ef1cc971e96b6c72fe6ccce86128ac3
muyawia <muyawia_kashif@outlook.com>
2022-08-25 19:07:24 +0100

    replace broken file

---
+++ Client/src/data/categories.js
@@ -0,0 +1,25 @@
+const categories = [
+  {
+    "name": "Ancient Times"
+  },
+  {
+    "name": "Dazzle"
+  },
+  {
+    "name": "Deer: In An Extra Dimension"
+  },
+  {
+    "name": "Nishikigoi Taijitu"
+  },
+  {
+    "name": "Salvador's Cows"
+  },
+  {
+    "name": "The S Collection"
+  },
+  {
+    "name": "Wolf God's"
+  },
+]
+
+export default categories;

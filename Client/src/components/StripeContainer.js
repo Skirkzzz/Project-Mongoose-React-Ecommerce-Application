19710800323e9210509162e8ee1f297cc2ce843f
muyawia <muyawia_kashif@outlook.com>
2022-08-27 05:26:30 +0100

    add stripe and checkout routes

--- Client/src/components/StripeContainer.js
+++ Client/src/components/StripeContainer.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+import {Checkout} from './pages/Checkout'
 
 const publicKey = "pk_test_51LbFtyIbkVz73ZrSygi1E33lW2LJ3Fjcyjns3YOsVBra8Fk5ExvR3s046ivRe6IA1xpfL930lHfJZ3t0YWPZ0ON700zbvInyso"
 
 const stripeTestPromise = loadStripe(publicKey)
 export default function StripeContainer() {
     return (
-        <div>
-
-        </div>
+        <Elements stripe={stripeTestPromise}>
+            <Checkout />
+        </Elements>
     )
 }
